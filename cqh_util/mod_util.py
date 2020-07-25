72094fd29608cd288c1316229127abc885d61ae6
cheqinghe <1832866299@qq.com>
2020-07-25 19:45:51 +0800

    add new method to mod_util

--- cqh_util/mod_util.py
+++ cqh_util/mod_util.py
@@ -1,5 +1,50 @@
 
 
+def mod_record_map(mod, input_list, field,
+                   input_field=None):
+    """
+    一般用于表的关联查询
+    比如user的detail信息 根据user_list, 提取用户id,也就是用户表的主键id, 然后在detail表里面找到user_id={user_id}的记录，然后做成一个map
+        上面这个例子, mod:是Detail, input_list: userlist, input_field: id, field: user_id
+
+    """
+    if not input_list:
+        return dict()
+    if not isinstance(input_list[0], (str, int, float, bytes)):
+        assert input_field is not None, "input_field should not be None when input_list[0] is instance of {}".format(type(input_list[0]))
+        if isinstance(input_list[0], dict):
+            input_list = [e[input_field] for e in input_list]
+        else:
+            input_list = [getattr(e, input_field) for e in input_list]
+    query_set = mod.select().where([getattr(mod, field).in_ == input_list])
+    return {getattr(e, field): e for e in query_set}
+
+
+def mod_get_in_db_set(mod,
+                      input_list,
+                      field,
+                      format=None):
+    """
+    找到input_list在数据库中存在的数据
+    一般用于prepare_data里面
+    用来替代:
+        for name in input_name:
+            if mod.get_or_none(name__eq=name):
+                continue
+        把多条sql转成一条
+
+    """
+    if not input_list:
+        return set()
+
+    def default_format(x):
+        return getattr(x, field)
+    if format is None:
+        format = default_format
+    query_set = mod.select().where([getattr(mod, field).in_ == input_list])
+    return {format(x) for x in query_set}
+
+
 def mod_record_cursor_id_between(mod, from_id, to_id,
                                  base_exression_list=None,
                                  page_size=10):
