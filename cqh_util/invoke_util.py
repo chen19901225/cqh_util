46c9d796a4289ad26f3135ee99f154e97ed34a49
test <test@qq.com>
2020-11-04 21:13:38 +0800

    fix bug

--- cqh_util/invoke_util.py
+++ cqh_util/invoke_util.py
@@ -1,4 +1,5 @@
 import git
+import os
 import typing
 
 
@@ -17,7 +18,16 @@ def git_unstaged_and_untracked_file_list(proj_dir: [str, git.Repo]) -> typing.Li
         file_set.add(diff.b_path)
     file_list = list(file_set)
     file_list = file_list + repo.untracked_files
-    return file_list
+    filter_file_list = []
+    proj_dir = repo.git_dir
+    if proj_dir.endswith("/.git"):
+        last_index = proj_dir.rindex("/")
+        proj_dir = proj_dir[:last_index]
+    for name in file_list:
+        abs_path = os.path.join(repo.git_dir, name)
+        if os.path.exists(abs_path):
+            filter_file_list.append(name)
+    return filter_file_list
 
 
 def git_current_branch(proj_dir: str) -> str:
