cb029de0824829a497920a89ffd4a6f528c4bdad
test <test@qq.com>
2020-12-11 17:51:39 +0800

    add logger_util

--- cqh_util/__init__.py
+++ cqh_util/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "0.0.21"
+# generated_by_export_class:logger_util||MultiLineLoggerAdapter
+from .logger_util import MultiLineLoggerAdapter
+
+# generated_by_export_class:logger_util||ensure_logger
+from .logger_util import ensure_logger
+
+__all__ = [
+    'MultiLineLoggerAdapter',
+    'ensure_logger'
+]
